23064d9fc10c099a7c306d663ac0c1b0e0dbcc87
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2025-10-25 12:42:18 +0200

    inform users about not having an ad-blocker installed

---
+++ ads/ad.js
@@ -0,0 +1,12 @@
+/*
+Does NOT contain any ads!
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This is just bait for ad-blocker!
+~AlbertUnruh
+*/
+
+document.getElementById("no-adblock").style.display = "block";
+
+console.log("Well, this could've been an ad which just loaded...")
+console.log("~AlbertUnruh")
